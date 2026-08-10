8e020c277402978970c7238e53553f9b2a5cef1f
psshrijith <shrijithps89@gmail.com>
2026-08-10 21:37:37 +0530

    feat(taskFlow): added signup page

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,9 +1,15 @@
+import {useState} from 'react';
 import NavBar from '../components/NavBar';
+import Signup from '../components/SignUp';
 
 const LandingPage = () => {
+  
+  const [openForm, setOpenForm] = useState(false);
+
   return (
     <div className="flex h-screen w-full bg-linear-to-b from-black to-white">
-      <NavBar/>
+      <NavBar setOpenForm={setOpenForm}/>
+      <Signup openForm={openForm}/>
     </div>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { FormattedMessage } from "react-intl";
 
-const NavBar = () => {
-    console.log("navbar");
+const NavBar = ({setOpenForm}) => {
+
     return (
         <nav className="flex w-full flex-row items-start justify-between text-white list-none border-b border-red-100 px-10">
                 <p className="text-white p-3 text-3xl">
@@ -12,7 +12,11 @@ const NavBar = () => {
                     <ul className="flex list-none p-3 rounded-lg gap-6 items-center">
                         <li>Product</li>
                         <li>Resources</li>
-                        <button className="px-3 py-2 bg-white text-black rounded-lg">SignUp</button>
+                        <button 
+                            className="px-3 py-2 bg-white text-black rounded-lg"
+                            onClick={() => setOpenForm(true)}
+                        >
+                        SignUp</button>
                     </ul>
                 </div>
             
