08225a8a65eecfde85033c374eb0233bcbca5612
psshrijith <shrijithps89@gmail.com>
2026-08-10 20:51:26 +0530

    feat(taskFlow): fixed gradient colors

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import NavBar from '../components/NavBar';
 
 const LandingPage = () => {
   return (
-    <div className="flex h-screen w-full bg-gradient-to-b from-black to-white">
+    <div className="flex h-screen w-full bg-linear-to-b from-black to-white">
       <NavBar/>
     </div>
   );
