24bef1eebc93a4ac4175063eee4fd42e37bb4f23
psshrijith <shrijithps89@gmail.com>
2026-08-11 22:00:15 +0530

    feat(taskFlow): fixed the build issue

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -6,7 +6,7 @@ const Signup = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSignUp = (event) => {
+    const handleSignUp = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
     };
 
