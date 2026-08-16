9a31b0db148eabf7b208bdea613d8143d9bec0d6
psshrijith <shrijithps89@gmail.com>
2026-08-16 17:48:57 +0530

    added theme and user context

--- src/context/User/UserContext.tsx
+++ src/context/User/UserContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import {createContext, useContext, useState, type ReactNode} from "react";
+import {createContext} from "react";
 
 type UserContextType = {
     names: string;
--- src/context/User/UserProvider.tsx
+++ src/context/User/UserProvider.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, useContext, useState} from "react";
+import { type ReactNode, useState} from "react";
 import {UserContext} from "./UserContext";
 
 type UserProviderProps = {
