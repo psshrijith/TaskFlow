af818fef223b566376e168f9b3ff140f228262bd
psshrijith <shrijithps89@gmail.com>
2026-08-16 10:28:16 +0530

    fixed the lint errors

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import SideBar from "../components/SideBar";
-import NavBar from "../components/NavBar";
 
 const Dashboard = () => {
-    const [open, setOpen] = useState(false);
 
     return (
         <div className="min-h-screen bg-gray-100">
