c5108996c72f54105bf287c4033505862ba8eb7e
psshrijith <shrijithps89@gmail.com>
2026-09-10 22:08:16 +0530

    feat(taskFlow): added protected route in the components

--- src/utils/validatePassword.ts
+++ src/utils/validatePassword.ts
@@ -1,9 +1,9 @@
 const validatePassword = (password: string) => {
     const hasLetter = /[a-zA-Z]/.test(password);
     const hasNumber = /[0-9]/.test(password);
-    const minLength = password.length >= 6;
+    const minLength = password.length >= 8;
 
-    if (!minLength) return "Password must be at least 6 characters long.";
+    if (!minLength) return "Password must be at least 8 characters long.";
     if (!hasLetter || !hasNumber) {
         return "Password must contain at least one letter and one number.";
     }
